6b83c3fff4d635d179e1dea30611292b74bd7eeb
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-28 00:23:17 +0530

    save

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
 
-
 function printFileContents(filePath) {
-  // TODO: Use fs.readFile to read the file contents
-  fs.readFile(filePath,(err, data)=>{
+  fs.readFile(filePath, (err, data) => {
     if(err){
-      console.log("Column '1' not found in the CSV.")
+      console.log(`Error: ${err.message}`);
     }
     else{
       console.log(data.toString());
     }
-  })
+  });
 }
-let filePath ='./output.txt';
-printFileContents(filePath)
-// TODO: Call printFileContents with the command-line argument
+
+let filePath = process.argv[2]; // Get the file path from command-line arguments
+console.log(filePath)
+printFileContents(filePath);
