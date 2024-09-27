60e8f881a05b47f1379229ffd400ebc0d9a4e3ed
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-27 23:34:56 +0530

    save

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 
+
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath,(err, data)=>{
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(data.toString());
+    }
+  })
 }
-
+let filePath ='./output.txt';
+printFileContents(filePath)
 // TODO: Call printFileContents with the command-line argument
